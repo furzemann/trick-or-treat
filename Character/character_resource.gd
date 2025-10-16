06e05de9dc002a2a053e00549f58a9bbacf30ea4
blueegreen <arinband42@gmail.com>
2025-10-17 01:00:23 +0530

    modification

--- Character/character_resource.gd
+++ Character/character_resource.gd
@@ -10,4 +10,6 @@ enum HEIGHT_TYPE {SHORT, MEDIUM, TALL, RANDOM}
 @export var is_full_outfit := false
 @export var monster_type : MONSTER_TYPE = MONSTER_TYPE.GENERIC
 @export var height : HEIGHT_TYPE = HEIGHT_TYPE.SHORT
-@export var dialogue : Array[String]
+@export var dialogue : String
+@export var treat_dialogue : String
+@export var trick_dialogue : String
