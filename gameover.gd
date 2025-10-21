7c0203face1d44b017b49ea37f8cbd3b9be23c04
eevx <155942309+eevx@users.noreply.github.com>
2025-10-21 15:18:42 +0530

    Reapply "Note on the description"
    This reverts commit de5a62300d1f3f14e609ff4b617a4d747e282bf6.

--- gameover.gd
+++ gameover.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 @export var main_game_scene : PackedScene
-
+@export var retry_button : Button
 func _ready() -> void:
 	$AnimationPlayer.play("label_show")
-
+	retry_button.pressed.connect(_on_retry_button_down)
 
 func _on_retry_button_down() -> void:
 	get_tree().change_scene_to_packed(main_game_scene)
