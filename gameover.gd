bdffaa0f070501bfe8f25dbd173b8c2dc122ed95
eevx <155942309+eevx@users.noreply.github.com>
2025-10-21 15:28:23 +0530

    See description
    Fixes to be made:-
    CANDYMAN GAME OVER SCENES (Done)
    CANDY LEFT BOUNDARY CONDITION
    CHILD SFX
    CANDYMAN SFX (Fixed)
    TRICKS MUSIC + SFX
    CANDY PARTICLES
    DIALOGUE GLITCH FIX

--- gameover.gd
+++ gameover.gd
@@ -1,10 +1,8 @@
 extends Node2D
 
-@export var main_game_scene : PackedScene
-
 func _ready() -> void:
 	$AnimationPlayer.play("label_show")
 
 
 func _on_retry_button_down() -> void:
-	get_tree().change_scene_to_packed(main_game_scene)
+	get_tree().change_scene_to_file('res://Main/GameScene.tscn')
